381c1bde9a1d4570e2e0db1e1a92c0152bfa2c4d
Junyoung Choi <fluke8259@gmail.com>
2017-06-25 23:13:13 +0900

    Delete meaningless type and set more ignore pattern

--- src/lib/createActionCreator.ts
+++ src/lib/createActionCreator.ts
@@ -7,7 +7,7 @@ import {
 export function createActionCreator <ACTION extends PayloadAction<ACTION['type'], ACTION['payload']>>(type: ACTION['type']): (payload: ACTION['payload']) => ACTION
 export function createActionCreator <ACTION extends PureAction<ACTION['type']>>(type: ACTION['type']): () => ACTION
 export function createActionCreator <ACTION extends PayloadAction<ACTION['type'], ACTION['payload']>>(type: ACTION['type']): (payload: ACTION['payload']) => ACTION {
-  return (payload: any) => (
+  return (payload) => (
     payload == null
       ? ({
         type,
