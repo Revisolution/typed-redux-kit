9b4479d8d9cf97fe9768b559903019345e7b7713
Junyoung Choi <fluke8259@gmail.com>
2017-08-02 08:15:08 +0900

    Re-organize modules
    To import batched-actions, both import statements are available now.
    import { BatchedActions } from 'typed-redux-kit'
    import * as BatchedActions from 'typed-redux-kit/batched-actions'

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,3 +3,6 @@ export * from './createActionCreator'
 export * from './mapped-reducer'
 export * from './mapped-pipe-reducer'
 export * from './mapped-unique-pipe-reducer'
+import * as BatchedActions from './batched-actions'
+
+export { BatchedActions }
