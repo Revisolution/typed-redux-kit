cbadc5873f5b1962a80ef455c1058ab85c79b22f
Junyoung Choi <fluke8259@gmail.com>
2017-08-12 18:47:58 +0900

    We love tango :dancer:

--- src/specs/trackable/array.spec.ts
+++ src/specs/trackable/array.spec.ts
@@ -2,179 +2,187 @@ import TrackableArray from '../../lib/trackable/array'
 
 describe('TrackableArray', () => {
   describe('constructor', () => {
-    it('converts from array', () => {
-      const arr = TrackableArray<number>([1])
+    it('converts from tangoay', () => {
+      const tango = TrackableArray<number>([1])
 
-      expect(arr[0]).toBe(1)
-      expect(arr.$trackable.isChanged).toBe(false)
+      expect(tango[0]).toBe(1)
+      expect(tango.$trackable.isChanged).toBe(false)
+    })
+  })
+
+  describe('length', () => {
+    it('presents length of array', () => {
+      const tango = TrackableArray<number>([1])
+
+      expect(tango.length).toBe(1)
     })
   })
 
   describe('set', () => {
     it('updates data and mark itself as changed', () => {
-      const arr = TrackableArray<number>([1])
+      const tango = TrackableArray<number>([1])
 
-      arr[0] = 2
+      tango[0] = 2
 
-      expect(arr[0]).toBe(2)
-      expect(arr.$trackable.isChanged).toBe(true)
+      expect(tango[0]).toBe(2)
+      expect(tango.$trackable.isChanged).toBe(true)
     })
 
     it('marks parent changed', () => {
-      const childArr = TrackableArray<number>([1])
-      const arr = TrackableArray<number[]>([
-        childArr,
+      const childTango = TrackableArray<number>([1])
+      const tango = TrackableArray<number[]>([
+        childTango,
       ])
 
-      childArr[0] = 2
+      childTango[0] = 2
 
-      expect(arr.$trackable.isChanged).toBe(true)
+      expect(tango.$trackable.isChanged).toBe(true)
     })
   })
 
   describe('push', () => {
     it('pushes value', () => {
-      const arr = TrackableArray<number>([1])
+      const tango = TrackableArray<number>([1])
 
-      const length = arr.push(2)
+      const length = tango.push(2)
 
       expect(length).toBe(2)
-      expect(arr[1]).toBe(2)
-      expect(arr.$trackable.isChanged).toBe(true)
+      expect(tango[1]).toBe(2)
+      expect(tango.$trackable.isChanged).toBe(true)
     })
 
     it('pushes multiple values', () => {
-      const arr = TrackableArray<number>([1])
+      const tango = TrackableArray<number>([1])
 
-      const length = arr.push(2, 3)
+      const length = tango.push(2, 3)
 
       expect(length).toBe(3)
-      expect(arr[1]).toBe(2)
-      expect(arr[2]).toBe(3)
-      expect(arr.$trackable.isChanged).toBe(true)
+      expect(tango[1]).toBe(2)
+      expect(tango[2]).toBe(3)
+      expect(tango.$trackable.isChanged).toBe(true)
     })
 
     it('marks parent changed', () => {
-      const childArr = TrackableArray<number>([1])
-      const arr = TrackableArray<number[]>([
-        childArr,
+      const childTango = TrackableArray<number>([1])
+      const tango = TrackableArray<number[]>([
+        childTango,
       ])
 
-      childArr.push(2)
+      childTango.push(2)
 
-      expect(arr.$trackable.isChanged).toBe(true)
+      expect(tango.$trackable.isChanged).toBe(true)
     })
   })
 
   describe('unshift', () => {
     it('unshifts value', () => {
-      const arr = TrackableArray<number>([1])
+      const tango = TrackableArray<number>([1])
 
-      const length = arr.unshift(0)
+      const length = tango.unshift(0)
 
       expect(length).toBe(2)
-      expect(arr[0]).toBe(0)
-      expect(arr.$trackable.isChanged).toBe(true)
+      expect(tango[0]).toBe(0)
+      expect(tango.$trackable.isChanged).toBe(true)
     })
 
     it('unshifts multiple values', () => {
-      const arr = TrackableArray<number>([1])
+      const tango = TrackableArray<number>([1])
 
-      const length = arr.unshift(-1, 0)
+      const length = tango.unshift(-1, 0)
 
       expect(length).toBe(3)
-      expect(arr[0]).toBe(-1)
-      expect(arr[1]).toBe(0)
-      expect(arr.$trackable.isChanged).toBe(true)
+      expect(tango[0]).toBe(-1)
+      expect(tango[1]).toBe(0)
+      expect(tango.$trackable.isChanged).toBe(true)
     })
 
     it('marks parent changed', () => {
-      const childArr = TrackableArray<number>([1])
-      const arr = TrackableArray<number[]>([
-        childArr,
+      const childTango = TrackableArray<number>([1])
+      const tango = TrackableArray<number[]>([
+        childTango,
       ])
 
-      childArr.unshift(0)
+      childTango.unshift(0)
 
-      expect(arr.$trackable.isChanged).toBe(true)
+      expect(tango.$trackable.isChanged).toBe(true)
     })
   })
 
   describe('pop', () => {
     it('pops a value', () => {
-      const arr = TrackableArray<number>([1, 2])
+      const tango = TrackableArray<number>([1, 2])
 
-      const value = arr.pop()
+      const value = tango.pop()
 
       expect(value).toBe(2)
-      expect(arr[0]).toBe(1)
-      expect(arr.$trackable.isChanged).toBe(true)
+      expect(tango[0]).toBe(1)
+      expect(tango.$trackable.isChanged).toBe(true)
     })
 
     it('marks parent changed', () => {
-      const childArr = TrackableArray<number>([1])
-      const arr = TrackableArray<number[]>([
-        childArr,
+      const childTango = TrackableArray<number>([1])
+      const tango = TrackableArray<number[]>([
+        childTango,
       ])
 
-      childArr.pop()
+      childTango.pop()
 
-      expect(arr.$trackable.isChanged).toBe(true)
+      expect(tango.$trackable.isChanged).toBe(true)
     })
   })
 
   describe('shift', () => {
     it('shifts a value', () => {
-      const arr = TrackableArray<number>([1, 2])
+      const tango = TrackableArray<number>([1, 2])
 
-      const value = arr.shift()
+      const value = tango.shift()
 
       expect(value).toBe(1)
-      expect(arr[0]).toBe(2)
-      expect(arr.$trackable.isChanged).toBe(true)
+      expect(tango[0]).toBe(2)
+      expect(tango.$trackable.isChanged).toBe(true)
     })
 
     it('marks parent changed', () => {
-      const childArr = TrackableArray<number>([1])
-      const arr = TrackableArray<number[]>([
-        childArr,
+      const childTango = TrackableArray<number>([1])
+      const tango = TrackableArray<number[]>([
+        childTango,
       ])
 
-      childArr.shift()
+      childTango.shift()
 
-      expect(arr.$trackable.isChanged).toBe(true)
+      expect(tango.$trackable.isChanged).toBe(true)
     })
   })
 
   describe('slice', () => {
-    it('returns a clean array', () => {
-      const arr = TrackableArray<number>([1])
+    it('returns a clean tangoay', () => {
+      const tango = TrackableArray<number>([1])
 
-      const newArr = arr.slice()
+      const newTango = tango.slice()
 
-      expect(Array.isArray(newArr)).toBe(true)
-      expect(newArr[0]).toBe(1)
+      expect(Array.isArray(newTango)).toBe(true)
+      expect(newTango[0]).toBe(1)
     })
   })
 
   describe('clone', () => {
-    it('returns a clean array', () => {
-      const arr = TrackableArray<number>([1])
-      arr[0] = 2
+    it('returns a clean tangoay', () => {
+      const tango = TrackableArray<number>([1])
+      tango[0] = 2
 
-      const newArr = arr.clone()
+      const newTango = tango.clone()
 
-      expect(newArr[0]).toBe(2)
-      expect(newArr.$trackable.isChanged).toBe(false)
+      expect(newTango[0]).toBe(2)
+      expect(newTango.$trackable.isChanged).toBe(false)
     })
   })
 
   describe('reserveArrayBuffer', () => {
     it('reserves more buffer on demand', () => {
       const hugeArray = Array.from({length: 1001}).map((v, k) => k)
-      const arr = TrackableArray<number>(hugeArray)
+      const tango = TrackableArray<number>(hugeArray)
 
-      expect(arr[1000]).toBe(1000)
+      expect(tango[1000]).toBe(1000)
     })
   })
 })
